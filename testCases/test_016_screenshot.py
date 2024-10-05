6d6afbab1ea5700e4a43b11b1e4c9a9c7a4128a5
pallavivshinde <shindepallavi506@gmail.com>
2024-10-05 17:55:50 +0530

    first commit of ss_rev

--- testCases/test_016_screenshot.py
+++ testCases/test_016_screenshot.py
@@ -8,36 +8,32 @@ class Test_016_Screenshot():
         driver.implicitly_wait(10)
         driver.maximize_window()
 
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        driver.get("https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS9jdXN0b21lci9hY2NvdW50L2xvZ291dFN1Y2Nlc3Mv/")
         time.sleep(25)
 
-        driver.find_element(By.XPATH,"//input[@placeholder='Username']").send_keys("Admin")
+        driver.find_element(By.XPATH,'//input[@id="email"]').send_keys("pallavirg@gmail.com")
         time.sleep(3)
 
-        driver.find_element(By.XPATH,'//input[@name="password"]').send_keys("admin123")
+        driver.find_element(By.XPATH,'//input[@id="pass"]').send_keys("Pallavi123")
         time.sleep(3)
 
-        driver.find_element(By.XPATH,'//button[text()=" Login "]').click()
+        driver.find_element(By.XPATH,'(//button[@id="send2"])[1]').click()
         time.sleep(3)
 
-        try:
-            driver.find_element(By.XPATH,'//i[@class="oxd-icon bi-caret-down-fill oxd-userdropdown-icon"]')
-
-            print("\n ************* LOGIN IS DONE SUCCESSFULLY *********")
+        if(driver.title=="My Account"):
             driver.save_screenshot("C:\\Users\\lenovo\\PycharmProjects\\Revision2\\Sreenshot\\test_016_ss_pass.png")
+            print("\n-----------YOU ARE AT THE LOGIN PAGE-------------\n")
+            driver.find_element(By.XPATH,'(//button[@class="action switch"])[1]').click()
+            driver.find_element(By.XPATH,"(//a[normalize-space()='Sign Out'])[1]").click()
             time.sleep(3)
-            driver.find_element(By.XPATH, '//i[@class="oxd-icon bi-caret-down-fill oxd-userdropdown-icon"]').click()
-            time.sleep(3)
-            driver.find_element(By.XPATH,'//a[text()="Logout"]').click()
-            time.sleep(3)
-            assert True
             driver.close()
-        except:
-            print("\n ************* LOGIN IS FAILED *********")
+            assert True
+        else:
             driver.save_screenshot("C:\\Users\\lenovo\\PycharmProjects\\Revision2\\Sreenshot\\test_016_ss_fail.png")
-            time.sleep(3)
-
+            print("\n-----------SORRY,SOMETHING WENT WRONG-------------\n")
+            driver.close()
             assert False
+
 
     def test_016_addition(self):
         a=10
